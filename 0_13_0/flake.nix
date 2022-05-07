dcd4c2f91af6d70126bbaae58dc925e584c54e4d
hugosenari <hugosenari@gmail.com>
2022-05-06 23:07:55 -0300

    chore: re index paratext

--- 0_13_0/flake.nix
+++ 0_13_0/flake.nix
@@ -7,30 +7,22 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-paratext-0_8_0.flake = false;
-  inputs.src-paratext-0_8_0.ref   = "refs/tags/0.8.0";
-  inputs.src-paratext-0_8_0.owner = "paranim";
-  inputs.src-paratext-0_8_0.repo  = "paratext";
-  inputs.src-paratext-0_8_0.type  = "github";
-  
-  inputs."paranim".owner = "nim-nix-pkgs";
-  inputs."paranim".ref   = "master";
-  inputs."paranim".repo  = "paranim";
-  inputs."paranim".dir   = "0_12_0";
-  inputs."paranim".type  = "github";
-  inputs."paranim".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."paranim".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  inputs.src-paratext-0_13_0.flake = false;
+  inputs.src-paratext-0_13_0.ref   = "refs/tags/0.13.0";
+  inputs.src-paratext-0_13_0.owner = "paranim";
+  inputs.src-paratext-0_13_0.repo  = "paratext";
+  inputs.src-paratext-0_13_0.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-paratext-0_8_0"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-paratext-0_13_0"];
     over = if builtins.pathExists ./override.nix 
            then { override = import ./override.nix; }
            else { };
   in lib.mkRefOutput (over // {
     inherit self nixpkgs ;
-    src  = deps."src-paratext-0_8_0";
+    src  = deps."src-paratext-0_13_0";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   } );
