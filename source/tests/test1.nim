0cdea8dc747d95ba68bacdc0f0c4da48ebe8e968
hugosenari <hugosenari@gmail.com>
2022-09-03 19:50:29 -0300

    feat(nimpkgs): update lock

--- source/tests/test1.nim
+++
@@ -1,8 +0,0 @@
-const ttf = staticRead("Roboto-Regular.ttf")
-
-import unittest
-
-import paratext
-
-test "load font":
-  discard initFont(ttf = ttf, fontHeight = 64, firstChar = 32, bitmapWidth = 512, bitmapHeight = 512, charCount = 2048)
